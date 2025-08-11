a1294fc97dddc5f1ee7ab3eeaf5d03d333eea793
Alimp <A.Mohsenpour@sgas.ir>
2025-08-11 18:33:23 +0330

    Make auth for admin

---
+++ pages/auth/admin.vue
@@ -0,0 +1,65 @@
+<template>
+   <div class="w-100 flex flex-column fade-animation-1s">
+        <div class="w-100 flex flex-column justify-start">
+            <span class="f-s-16 f-w-500">Welcome back to admin pannel</span>
+            <span class="f-s-12 f-w-100 color-primary pt-5">Please authentication with username and password</span>
+            </div>
+            <BaseInput 
+                validate="true" 
+                v-model:access="access" 
+                v-model="form.username" 
+                rules="length"
+                min-length="8"
+                max-length="15"
+                class="mt-15" 
+                label="Username" 
+                placeholder="Please Enter Username"
+            />
+            <BaseInput 
+                validate="true" 
+                v-model:access="access" 
+                v-model="form.password" 
+                rules="length"
+                min-length="8"
+                max-length="15"
+                class="mt-15" 
+                label="Password" 
+                placeholder="Please Enter Password"
+            />
+            <BaseButton 
+                class="mt-15" 
+                name="Send" 
+                width="100%" 
+                height="40px" 
+                :loading="loading" 
+                :disabled="disabled" 
+                @click="login"
+            />
+   </div>
+</template>
+
+<script setup>
+import { userController } from '@/controllers/User'
+
+definePageMeta({
+  layout: "auth",
+});
+
+const access = ref(false)
+const loading = ref(false)
+
+const form = ref({
+    username: "",
+    password: "",
+})
+
+const disabled = computed(() => {
+   return !access.value || !form.value.username || !form.value.password ? true : false
+})
+
+const login = async () => {
+    loading.value = !loading.value
+    await userController.requestOtp(from.value)
+    loading.value = !loading.value
+}
+</script>
