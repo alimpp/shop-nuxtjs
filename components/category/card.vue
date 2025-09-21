51e1aaae705cf4acbc4d1969c3e32f2c0241d324
Alimp <A.Mohsenpour@sgas.ir>
2025-09-21 15:51:44 +0330

    Update category cardf

--- components/category/card.vue
+++ components/category/card.vue
@@ -3,26 +3,42 @@
     <div class="flex flex-column">
       <div class="flex">
         <div class="flex w-50">
-          <span class="f-s-14 f-w-500 color-gray">Creator</span>
+          <span class="f-s-14 f-w-500">Creator</span>
         </div>
         <div class="flex w-50 justify-end pt-5">
-          <BaseIcon
-            name="solar:pen-linear"
-            size="17"
-            class="cursor-pointer mx-5"
+          <BaseIconContent
+            class="mx-4"
+            bgClass="bg-success"
+            width="25px"
+            height="25px"
             v-if="!updateState"
-            @click="changeUpdateState"
-          />
-          <BaseIcon
-            name="line-md:menu-to-close-alt-transition"
-            size="17"
-            class="cursor-pointer"
-          />
+          >
+            <BaseIcon
+              name="solar:pen-linear"
+              size="17"
+              class="cursor-pointer mx-5"
+              @click="changeUpdateState"
+            />
+          </BaseIconContent>
+          <BaseIconContent
+            bgClass="bg-red"
+            width="25px"
+            height="25px"
+            v-if="!updateState"
+          >
+            <BaseIcon
+              name="line-md:menu-to-close-alt-transition"
+              size="17"
+              class="cursor-pointer"
+            />
+          </BaseIconContent>
         </div>
       </div>
       <div class="flex align-center">
-        <BaseIcon name="solar:user-linear" size="20" />
-        <span class="f-s-14 f-w-600 pt-5 px-5">{{ data.submiter }}</span>
+        <BaseIcon name="solar:user-linear" size="20" color="#7d7be5" />
+        <span class="f-s-14 f-w-600 pt-5 px-5 color-primary">{{
+          data.submiter
+        }}</span>
       </div>
       <BaseDivider />
       <div class="flex flex-column" v-if="updateState">
@@ -38,13 +54,13 @@
         <div class="flex py-10">
           <BaseButton
             name="Update"
-            bg="bg-primary"
+            bg="bg-success"
             icon="gg:check-o"
             :disabled="!access"
           />
           <BaseButton
             name="Cancel"
-            bg="bg-danger"
+            bg="bg-red"
             class="mx-5"
             icon="line-md:close"
             @click="updateState = !updateState"
@@ -52,8 +68,8 @@
         </div>
       </div>
       <div class="flex align-center" v-else>
-        <BaseIcon name="iconamoon:category-thin" size="25" />
-        <span class="f-s-15 f-w-600 pt-5 px-5">{{ data.name }}</span>
+        <BaseIcon name="iconamoon:category-thin" size="25" color="#9e9393" />
+        <span class="f-s-15 f-w-600 pt-5 px-5 color-gray">{{ data.name }}</span>
       </div>
     </div>
   </BaseCard>
