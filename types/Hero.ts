9b507a89b75be22da5b605b6182ee6047d7c78f7
Alimp <A.Mohsenpour@sgas.ir>
2025-11-29 16:50:47 +0330

    Add hero model and types

---
+++ types/Hero.ts
@@ -0,0 +1,10 @@
+export interface IHeroResponseServer {
+  id: string;
+  imageId: string;
+  title: string;
+  subTitle: string;
+  descrption: string;
+  redirect: string;
+  active: boolean;
+  created_at: string | Date;
+}
